ec02bd6b54e7e1c7443911a77c119dd3c0cbe9d6
Qasim Abdullah <89213175+qasim-at-tci@users.noreply.github.com>
2023-10-04 17:43:34 +0000

    ...

--- hello.sh
+++ hello.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+set -o xtrace
+
 docker pull nginx
 docker run -d -p 8080:80 nginx
-curl -I http://localhost:8080
-
+ping -c3 travis-ci.com
+touch test.log
+echo "Hello, world!" > test.log
+sleep 10
+cat test.log
